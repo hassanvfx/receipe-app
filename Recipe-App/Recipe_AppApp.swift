dc187803a034c0ecbc89a6933a0a2c16e786d979
Hassan Uriostegui <hassan.uriostegui@gmail.com>
2025-01-14 16:29:33 -0600

    added gitginore and initial xcode project

---
+++ Recipe-App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe-App
+//
+//  Created by Hassan on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe-AppTests/Recipe_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe_AppTests.swift
+//  Recipe-AppTests
+//
+//  Created by Hassan on 14/01/25.
+//
+
+import Testing
+
+struct Recipe_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
